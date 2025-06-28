c476db478c00a570f5c47d4f56c4e08f9e859c3b
Andrés Valdivieso Sorando <andres24vs@gmail.com>
2025-06-28 18:53:16 -0400

    REF: Pruebas mejoradas

--- tests/getWeatherApi.test.js
+++ tests/getWeatherApi.test.js
@@ -17,16 +17,28 @@ describe("Prueba de la ruta GET /weather/:source?city=[nombre_ciudad]", () => {
             "/weather/fuenteInvalida?city=CiudadInexistente"
         );
         expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty(
+            "error",
+            "La fuente de datos 'fuenteInvalida' no es válida. Las fuentes válidas son: OpenWeatherMap, WeatherApi o no colocar fuente y acceder a la Base de Datos propia."
+        );
     });
     it("debería responder 400 cuando no indican ciudad", async () => {
         const res = await request(app).get("/weather/openweathermap");
         expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty(
+            "error",
+            "Debe indicar el parámetro 'city' en la consulta."
+        );
     });
     it("debería responder 404 cuando pone una ciudad inexistente y se comunica con openweathermap", async () => {
         const res = await request(app).get(
             "/weather/openweathermap?city=CiudadInexistente"
         );
         expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty(
+            "error",
+            "La ciudad no fue encontrada en OpenWeatherMap"
+        );
     });
     it("debería responder 200 y dar resultado cuando colocas una ciudad y se comunica con openweathermap", async () => {
         const res = await request(app).get(
@@ -61,6 +73,10 @@ describe("Prueba de la ruta GET /weather/:source?city=[nombre_ciudad]", () => {
             "/weather/weatherapi?city=CiudadInexistente"
         );
         expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty(
+            "error",
+            "La ciudad no fue encontrada en WeatherAPI"
+        );
     });
     it("debería responder 200 y dar resultado cuando colocas una ciudad y se comunica con weatherapi sin importar mayusculas", async () => {
         const res = await request(app).get("/weather/WeatherAPI?city=Caracas");
@@ -90,9 +106,6 @@ describe("Prueba de la ruta GET /weather/:source?city=[nombre_ciudad]", () => {
 
         const res = await request(app).get("/weather?city=Caracas");
         expect(res.statusCode).toBe(404);
-        expect(res.body).toHaveProperty(
-            "mensaje",
-            "No hay registros climáticos"
-        );
+        expect(res.body).toHaveProperty("error", "No hay registros climáticos");
     });
 });
