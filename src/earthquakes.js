f728a382ec6206784f488e9863306005f2a2efe0
niwed18112 <eli.21@est.ucab.edu.ve>
2025-07-06 22:11:19 -0400

    ADD: Ruta de delete

--- src/earthquakes.js
+++ src/earthquakes.js
@@ -1,35 +1,8 @@
-import mongoose from "mongoose";
-import { Counter } from "./counter.js";
-const Schema = mongoose.Schema;
+const express = require('express');
+const { deleteEarthquakeById } = require('./earthquakeController');
+const { validateEarthquake } = require('./validate');
 
-const earthquakesSchema = new Schema({
-    _id: { type: String },
-    magnitude: { type: Number, required: true },
-    depth: { type: Number, required: true },
-    location: { type: String, required: true },
-    date: { type: Date, required: true },
-});
+const router = express.Router();
+router.delete('/:id', deleteEarthquakeById);
 
-earthquakesSchema.pre("save", function (next) {
-    if (this.isNew) {
-        Counter.findByIdAndUpdate(
-            { _id: "earthquakesId" },
-            { $inc: { seq: 1 } },
-            { new: true, upsert: true }
-        )
-            .then((counter) => {
-                this._id = `id:sismo_${counter.seq}`;
-                next();
-            })
-            .catch((error) => {
-                console.error("Error en el middleware pre-save:", error);
-                next(error);
-            });
-    } else {
-        next();
-    }
-});
-
-const Earthquakes = mongoose.model("Earthquakes", earthquakesSchema);
-
-export { Earthquakes };
+module.exports = router;
