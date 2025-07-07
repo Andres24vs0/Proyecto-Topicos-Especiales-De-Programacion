295b49e270a435278e22a141f86ca0f8bcf03291
niwed18112 <eli.21@est.ucab.edu.ve>
2025-07-06 20:14:00 -0400

    Delete src/Counter.js

--- src/Counter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CounterSchema = new mongoose.Schema({
-  _id: String,
-  seq: Number
-});
-
-module.exports = mongoose.model('Counter', CounterSchema);
